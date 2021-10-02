5eddc23fa1cc0d0ceccbfce112923680599a07b3
tokogit24 <82769087+tokogit24@users.noreply.github.com>
2021-10-02 16:43:38 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_s3_bucket" "btokoet" {
   bucket = "my-test-bucket"
   acl    = "public"
